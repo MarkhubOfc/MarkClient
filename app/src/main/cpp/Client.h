ae46ae4224cf9b048121bff626f3f21314302c0c
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-05-16 19:13:44 -0300

    Update Client.h

--- app/src/main/cpp/Client.h
+++ app/src/main/cpp/Client.h
@@ -3,26 +3,27 @@
 #include <android/native_window.h>
 
 class ModuleManager;
-class ImGuiManager;
+class UIManager;
 
 class Client {
 public:
-  static constexpr const char* NAME = "Mark Client";
-  static constexpr const char* VERSION = "1.0.0";
-  static Client& get();
-  void init();
-  void initImGui(ANativeWindow* window, int width, int height);
-  void shutdown();
-  void tick();
-  void render();
-  void toggleMenu();
-  ModuleManager* getModuleManager() { return moduleManager.get(); }
-  ImGuiManager* getImGuiManager() { return imguiManager.get(); }
+    static constexpr const char* NAME = "Mark Client";
+    static constexpr const char* VERSION = "1.0.0";
+    static Client& get();
+    void init();
+    void initImGui(ANativeWindow* window, int width, int height, void* assetMgr);
+    void shutdown();
+    void tick();
+    void render();
+    void toggleMenu();
+    ModuleManager* getModuleManager() { return moduleManager.get(); }
+    UIManager* getUIManager() { return uiManager.get(); }
+
 private:
-  Client() = default;
-  ~Client() = default;
-  std::unique_ptr<ModuleManager> moduleManager;
-  std::unique_ptr<ImGuiManager> imguiManager;
-  bool initialized = false;
-  bool imguiInitialized = false;
+    Client() = default;
+    ~Client() = default;
+    std::unique_ptr<ModuleManager> moduleManager;
+    std::unique_ptr<UIManager> uiManager;
+    bool initialized = false;
+    bool imguiInitialized = false;
 };
